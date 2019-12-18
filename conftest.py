b06802f37b3242704a5fd4a8164383a827e96bae
Юрий Виноградов <vinogradov@itoolabs.com>
2019-12-18 19:59:35 +0300

    delete firefox from conftest

--- conftest.py
+++ conftest.py
@@ -3,23 +3,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
-                     help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default="en",
                      help="Choose language: es, fr, en, ...")
 
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
-
-    browser_name = request.config.getoption("browser_name")
-    if browser_name == "firefox":
-        print("\nstart firefox browser for test")
-        browser = webdriver.Firefox()
-    else:
-        print("\nstart chrome browser for test")
-        browser = webdriver.Chrome()
-    
+   
     try:
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': language})
