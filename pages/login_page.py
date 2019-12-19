02ab7a072967add041ada7def792dd2a36cd319d
Юрий Виноградов <vinogradov@itoolabs.com>
2019-12-19 19:47:15 +0300

    Add new page object - login_page

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        assert self.browser.current_url == link, "Login link url not correct"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
