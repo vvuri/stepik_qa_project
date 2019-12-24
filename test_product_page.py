031804e3f193c50fb1748a706ee3271c37e102bd
Юрий Виноградов <vinogradov@itoolabs.com>
2019-12-24 18:04:39 +0300

    Two test with 2 links

--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,17 @@
 from .pages.product_page import ProductPage
 
-def  test_guest_can_add_product_to_basket(browser):
-    # link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.check_name_product("The shellcoder's handbook")
+    page.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+
+def test_guest_can_add_product_to_basket_2019(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.check_name_product("Coders at Work")
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
-
