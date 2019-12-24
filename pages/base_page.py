11256aa30f6c2a34eace506dd43a16a94e123ce1
vvuri <tempos78@mail.ru>
2019-12-24 22:59:18 +0300

    Add present and disappeared method

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,8 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException # в начале файла
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import math
 
 class BasePage():
@@ -31,3 +34,20 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    # упадет, как только увидит искомый элемент. Не появился: успех, тест зеленый. 
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    # будет ждать до тех пор, пока элемент не исчезнет
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
--- pages/product_page.py
+++ pages/product_page.py
@@ -5,8 +5,17 @@ class ProductPage(BasePage):
     def check_name_product(self):
         name_product = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT_BOOK)
         name_product_in_basket = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT_BOOK_IN_BASKET)
-        assert name_product.text == name_product_in_basket.text
+        assert name_product.text == name_product_in_basket.text, "Product name in basket and page not the same"
 
     def add_product_to_basket(self):
         add_to_basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
         add_to_basket.click()
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_be_disappeared_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is disappeared"
+        
