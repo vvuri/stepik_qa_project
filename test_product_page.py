20bb9b521e00b327a092b0f6299a2f6e0df28f79
Юрий Виноградов <vinogradov@itoolabs.com>
2019-12-24 18:22:59 +0300

    Update two test to one with param fixture

--- test_product_page.py
+++ test_product_page.py
@@ -1,17 +1,18 @@
 from .pages.product_page import ProductPage
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.fixture(scope="function", params=[
+("http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear", "The shellcoder's handbook"),
+("http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019", "Coders at Work")
+])
+def param_test(request):
+    return request.param
+
+def test_guest_can_add_product_to_basket(browser, param_test):
+    (link, name) = param_test
     page = ProductPage(browser, link)
     page.open()
-    page.check_name_product("The shellcoder's handbook")
+    page.check_name_product(name)
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
 
-def test_guest_can_add_product_to_basket_2019(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, link)
-    page.open()
-    page.check_name_product("Coders at Work")
-    page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
